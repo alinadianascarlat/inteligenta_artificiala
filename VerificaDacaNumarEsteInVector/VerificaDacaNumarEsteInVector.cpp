57d2c0446c5ecd5a3775cbf8fa5087261872aef6
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-02-23 11:47:19 +0200

    Rezolvat problema cautat un nr intr-un vector

--- VerificaDacaNumarEsteInVector/VerificaDacaNumarEsteInVector.cpp
+++ VerificaDacaNumarEsteInVector/VerificaDacaNumarEsteInVector.cpp
@@ -1,25 +1,34 @@
 //Verificam daca un nr este intr un vector
 
-
 #include <iostream>
 using namespace std;
 
 int main()
 {
+	int v[30], i, n, m;
+	cout << " n = ";
+	cin >> n;
 
-    int v[30], i, n, m;
-    cout << " n = ";
-    cin >> n;
-    cout << " m = ";
-    cin >> m;
-    for (i = 0; i < n; i++) {
-        cout << " v [" << i << " ] \n";
+	cout << " m = ";
+	cin >> m;
 
-    }
-    if (n == m) {
-        cout << " Numarul face parte din vector ";
-    }
-    else
-        cout << "Numarul nu face parte din vector ";
+	for (i = 0; i < n; i++) {
+		cout << "v[" << i << "] = ";
+		cin >> v[i];
+	}
+
+	bool gasit = false;
+	for (i = 0; i < n; i++) {
+		if (v[i] == m) {
+			gasit = true;
+			break;
+		}
+	}
+
+	if (gasit) {
+		cout << "Numarul este in vector \n";
+	}
+	else {
+		cout << "Numarul nu este in vector \n";
+	}
 }
-
