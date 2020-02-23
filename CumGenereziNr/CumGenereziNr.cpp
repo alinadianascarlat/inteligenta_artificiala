68a47623033c145414dcc390ed464294538323a4
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-02-23 12:03:26 +0200

    Rezolvare problema generare numere

--- CumGenereziNr/CumGenereziNr.cpp
+++ CumGenereziNr/CumGenereziNr.cpp
@@ -1,16 +1,15 @@
 // 11. Sa se genereze un numar intre 0 si 100 in C.
 
 #include <iostream>
+#include <time.h> 
+
 using namespace std;
+
 int main()
 {
-    int n, i;
-    cout << " n = ";
-    cin >> n;
-    for (i = 0; i < 10; i++) {
-        cout << (rand() % 101) << " \t";
-    }
+    srand(time(NULL));
 
-
+    cout << (rand() % 100) + 1;
+    
     return 0;
 }
