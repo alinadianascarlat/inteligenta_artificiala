29e4e6e5f3e0e3a1b5f98f08fb0b9aaa9b75b656
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-02-23 12:11:03 +0200

    Rezolvare problema ordonare elemente dintr-o lista

--- OrdonareaElementelorUnuiVector/OrdonareaElementelorUnuiVector.cpp
+++ OrdonareaElementelorUnuiVector/OrdonareaElementelorUnuiVector.cpp
@@ -4,22 +4,26 @@
 using namespace std;
 int main()
 {
-    int v[20], i, n, temp;
-    cout << "n = ";
-    cin >> n;
-    for (int i = 0; i < n; i++) {
-        cout << " v[ " << i << "] =";
-        cin >> v[i];
-    }
-    for (int i = 0; i < n - 1; i++)
-        for (int j = i + 1; j < n; j++)
-            if (v[i] > v[j]) {
-                temp = v[i];
-                v[i] = v[j];
-                v[j] = temp;
-            }
-    for (int i = 0; i < n; i++) {
-        cout << " v[ " << i << "] \n";
-    }
+	int v[20], i, n, temp;
+
+	cout << "n = ";
+	cin >> n;
+
+	for (int i = 0; i < n; i++) {
+		cout << "v[" << i << "] = ";
+		cin >> v[i];
+	}
+
+	for (int i = 0; i < n - 1; i++)
+		for (int j = i + 1; j < n; j++)
+			if (v[i] > v[j]) {
+				temp = v[i];
+				v[i] = v[j];
+				v[j] = temp;
+			}
+	
+	for (int i = 0; i < n; i++) {
+		cout << v[i] << endl;
+	}
 }
 
