6d974826c2e23c174d6ffcd7b5935307ac0f3206
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-02-23 11:13:59 +0200

    Terminat problema minim dintr-o lista

--- ValoareMinimaDintroLista/ValoareMinimaDintroLista.cpp
+++ ValoareMinimaDintroLista/ValoareMinimaDintroLista.cpp
@@ -7,19 +7,21 @@ int main()
 {
 
     int v[50], i, n, min;
+
     cout << " n = ";
     cin >> n;
-    for (i = 1; i <= n; i++) {
-        cout << v[i];
+    for (i = 0; i < n; i++) {
+        cout << "v[" << i << "]=";
+        cin >> v[i];
     }
-    min = v[1];
-    for (i = 2; i <= n; i++) {
+
+    min = v[0];
+    for (i = 1; i < n; i++) {
         if (v[i] < min) {
             min = v[i];
-
         }
     }
-    cout << " Minimul este " << min;
+    cout << "Minimul este " << min;
     return 0;
 }
 
