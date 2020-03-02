77f8d27ffb1f768bce4f8e1637e4568c21fc2643
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-03-02 11:20:28 +0200

    Rezolvarea problemei

--- EliminareaUnuiNrDePeOPozitieDinLista/EliminareaUnuiNrDePeOPozitieDinLista.cpp
+++ EliminareaUnuiNrDePeOPozitieDinLista/EliminareaUnuiNrDePeOPozitieDinLista.cpp
@@ -1,25 +1,42 @@
 //5. Sa se elimine un numar aflat pe o anumita pozitie intr - o lista.
 
 #include<iostream>
+
 using namespace std;
+
+void afisare(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "v[" << i << "] = " << a[i] << endl;
+	}
+}
+
+void citire(int a[], int n) {
+	for (int i = 0; i < n; i++) {
+		cout << "v[" << i << "] = ";
+		cin >> a[i];
+	}
+}
+
 int  main() {
+	int n, i, v[20], poz;
 
-	int n, i, p, v[20];
-
-	cout << " n = ";
+	cout << "n = ";
 	cin >> n;
-	
 
-	for ( i = 0; i < n; i++) {
-		cout << "v[" << i << "]=";
-		cin >> v[i];
-	}
+	cout << "Introducem pozitia de pe care eliminam nr: ";
+	cin >> poz;
 
-	for ( i = 0; i < n; i++) {
+	// n = 3
+	citire(v, n);
+
+	// 5 6 7 9 4
+	// poz 2 => 5 7 9 4
+	for (i = poz; i < n; i++) {
 		v[i] = v[i + 1];
-		n--;
 	}
-	for (i = 0; i < n; i++) {
-		cout << v[i];
-	}
+
+	n = n - 1;
+	// n = 2
+
+	afisare(v, n);
 }
