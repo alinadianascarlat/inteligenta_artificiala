f9e334adc88ac361a85bca806659c2c6f82a6f11
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-03-02 11:26:02 +0200

    Sters duplicat

---
+++ EliminareNrDintroListaData/EliminareNrDintroListaData.cpp
@@ -0,0 +1,27 @@
+//6. Sa se elimine un numar dat dintr-o lista data.
+
+
+// lista = 4 5 9 7
+// numarul = 9
+// 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// cazuri limita: 
+// 1. numarul nu exista in lista => afiseaza mesaj ca nu exista
+// 2. numarul exista de mai multe ori in lista => elimina toate prezentele in lista
