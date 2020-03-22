5874f1fab255b74fd254bfb710a5d4ca241bde8c
Alina Scarlat <alina.diana.scarlat@gmail.com>
2020-03-22 15:27:56 +0200

    Cost Uniform + Citire nod inceput/tinta

--- CautareNoduriAgentAmerican/CautareNoduriAgentAmerican.cpp
+++ CautareNoduriAgentAmerican/CautareNoduriAgentAmerican.cpp
@@ -8,8 +8,8 @@ private:
 		"Iasi","Lugoj","Mehadia","Neamt","Oradea","Pitesti","Ramnicul Valcea","Sibiu","Timisoara","Urziceni","Vaslui","Zerind" };
 	int a[20][20];
 	int parents[20];
-	int startNode = 0;
-	int targetNode = 1;
+	int startNode;
+	int targetNode;
 
 public:
 	void initializeAdiacenceMatrix() {
@@ -50,6 +50,22 @@ public:
 		}
 	}
 
+	void readStartEndCities() {
+		cout << "Introduceti numarul corespunzator orasului de plecare: " << endl;
+		for (int i = 0; i < 20; i++) {
+			cout << i << " - " << names[i] << endl;
+		}
+		cin >> this->startNode;
+
+		cout << "Introduceti numarul corespunzator orasului de sosire: " << endl;
+		for (int i = 0; i < 20; i++) {
+			if (i != this->startNode) {
+				cout << i << " - " << names[i] << endl;
+			}
+		}
+		cin >> this->targetNode;
+	}
+
 	void breathSearch() {
 		int visitedCities[20];
 		int nodesTail[20];
@@ -61,7 +77,7 @@ public:
 			parents[i] = -1;
 		}
 
-		nodesTail[nodesTailPos] = startNode; // pleaca din Arad
+		nodesTail[nodesTailPos] = startNode;
 		nodesTailPos++;
 		visitedCities[startNode] = 1;
 
@@ -82,7 +98,7 @@ public:
 						nodesTail[nodesTailPos] = i;
 						nodesTailPos++;
 						parents[i] = node;
-						visitedCities[node] = 1;
+						visitedCities[i] = 1;
 					}
 				}
 			}
@@ -99,14 +115,14 @@ public:
 			visitedCities[i] = 0;
 			parents[i] = -1;
 		}
-		nodesStack[nodesStackPos] = startNode; // pleaca din Arad
+		nodesStack[nodesStackPos] = startNode;
 		nodesStackPos++;
 		visitedCities[startNode] = 1;
 
 		while (!solutionFound) {
-			int node = nodesStack[nodesStackPos-1];
+			int node = nodesStack[nodesStackPos - 1];
 			nodesStackPos--;
-			
+
 			if (node == targetNode) {
 				solutionFound = true;
 			}
@@ -118,6 +134,45 @@ public:
 						nodesStackPos++;
 						parents[i] = node;
 						visitedCities[i] = 1;
+					}
+				}
+			}
+		}
+	}
+
+	void costUniform() {
+		int cost[20];
+		int nodesTail[20];
+		int nodesTailPos = 0;
+		bool solutionFound = false;
+
+		for (int i = 0; i < 20; i++) {
+			cost[i] = 0;
+			parents[i] = -1;
+		}
+
+		nodesTail[nodesTailPos] = startNode;
+		nodesTailPos++;
+		cost[startNode] = 0;
+
+		while (!solutionFound && nodesTailPos != 0) {
+			int node = nodesTail[0];
+
+			for (int i = 0; i < nodesTailPos; i++) {
+				nodesTail[i] = nodesTail[i + 1];
+			}
+			nodesTailPos--;
+
+			if (node == targetNode) {
+				solutionFound = true;
+			}
+			else {
+				for (int i = 0; i < 20; i++) {
+					if (a[i][node] != 0 && (cost[i] == 0 || cost[i] > cost[node] + a[i][node])) {
+						nodesTail[nodesTailPos] = i;
+						nodesTailPos++;
+						parents[i] = node;
+						cost[i] = cost[node] + a[i][node];
 					}
 				}
 			}
@@ -155,15 +210,21 @@ private:
 int main()
 {
 	FindPath* findPath = new FindPath();
+	findPath->readStartEndCities();
 	findPath->initializeAdiacenceMatrix(); // initializeaza matricea de adiacenta
 
-	cout << "Breath search" << endl;
+	cout << "Cautare in latime" << endl;
 	findPath->breathSearch(); // cauta in latime
 	findPath->printSolution(); // afiseaza solutia daca exista, altfel afiseaza un mesaj ca solutia nu exista
 	findPath->printTime();
 
-	cout << "Depth search" << endl;
+	cout << "Cautare in adancime" << endl;
 	findPath->depthSearch(); // cauta solutie in adancime
+	findPath->printSolution();
+	findPath->printTime();
+
+	cout << "Cautare Cost Uniform" << endl;
+	findPath->costUniform(); // cauta solutie cost uniform
 	findPath->printSolution();
 	findPath->printTime();
 }
